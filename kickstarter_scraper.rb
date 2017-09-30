668c8a87b4f0236202bfab3d01952476ce4e5fe0
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-09-30 20:11:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,13 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  #binding.pry
+
+  projects = {}
+
+  #iterate through the projects 
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end 
 end
 create_project_hash
