87b3aa3a9ca8003d40db2462632c3d12041358d5
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-09-30 20:12:57 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,8 @@ def create_project_hash
 
   #iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    
   end
 
   project
